90b5b334eb74dc7641742475b9582474f3ab29c8
mitsuki <mitsuki3553@gmail.com>
2023-05-24 22:33:17 +0900

    リファクタ

--- src/resources/js/components/Table2.jsx
+++ src/resources/js/components/Table2.jsx
@@ -6,33 +6,10 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import DBtest from '../api/DBtest';
 
 
 
 export default function CustomizedTables({item}) {
-  const [data, setData] = useState([]);
-
-  const [ location2, setLocation2 ] = useState(null);
-  const [ station, setStation ] = useState(null);
-  const [ land_area, setLand_area] = useState(null);
-  const [ build_ex_area, setBuild_ex_area] = useState(null);
-  const [ price, setPrice] = useState(null);
-  const [ use_area, setUse_area] = useState(null);
-    // データの取得
-    useEffect(() => {
-      fetchData();
-    }, []);
-  
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('/api/data');
-        setData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     return (
     <TableContainer component={Paper} sx={{ width:'100%' }}>
       <Table size="small" aria-label="a dense table">
@@ -89,8 +66,6 @@ export default function CustomizedTables({item}) {
                   </TableCell>
                   <TableCell align="left" sx={{ width: '70%' }}></TableCell>
             </TableRow>
-
-
         </TableBody>
       </Table>
     </TableContainer>
