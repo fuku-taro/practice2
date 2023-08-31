79843e4d0c930225d024e0bb9995bc9ebe2f34b6
fuku-taro <fukumitsu.seko@gmail.com>
2023-08-31 18:54:30 +0900

    save

--- src/resources/js/pages/Top copy.jsx
+++ src/resources/js/pages/Top copy.jsx
@@ -1,23 +1,18 @@
+import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import classes from "../../sass/top.module.scss";
 
-import AppBar from "@mui/material/AppBar";
+import styles from "../../sass/top.module.scss";
+
+
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import HomeIcon from "@mui/icons-material/Home";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 
-const pages = ["閲覧履歴", "お気に入り", "検索条件"];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
 const dummy = [
     [
         { id: 1, url:"/Search", title: "住まい", text: "00000件" },
@@ -32,218 +27,48 @@ const dummy = [
         { id: 8, url:"/Search", title: "その他", text: "8件" },
     ],
 ];
-
-export const Top_copy = () => {
-    const [anchorElNav, setAnchorElNav] = useState(null)
-    const [anchorElUser, setAnchorElUser] = useState(null)
-
-  const handleOpenNavMenu = (e) => {
-      setAnchorElNav(e.currentTarget);
-  };
-
-  const handleOpenUserMenu = (e) => {
-      setAnchorElUser(e.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-  };
-
-
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+export default function Top() {
   return (
-      <div className={classes.body}>
-          <div className={classes.header}>
-              <AppBar position="static">
-                  <Container maxWidth="xl">
-                      <Toolbar disableGutters>
-                          {/* 通常 */}
-                          <HomeIcon
-                              sx={{
-                                  display: { xs: "none", md: "flex" },
-                                  mr: 1,
-                              }}
-                          />
-                          <Typography
-                              variant="h6"
-                              noWrap
-                              component="a"
-                              href="/"
-                              sx={{
-                                  mr: 2,
-                                  display: { xs: "none", md: "flex" },
-                                  fontFamily: "monospace",
-                                  fontWeight: 700,
-                                  letterSpacing: ".3rem",
-                                  color: "inherit",
-                                  textDecoration: "none",
-                              }}
-                          >
-                              不動産（仮）
-                          </Typography>
-                          {/* 通常 */}
-                          {/* レスポンシブ */}
-                          <Box
-                              sx={{
-                                  flexGrow: 1,
-                                  display: { xs: "flex", md: "none" },
-                              }}
-                          >
-                              <IconButton
-                                  size="large"
-                                  aria-label="account of current user"
-                                  aria-controls="menu-appbar"
-                                  aria-haspopup="true"
-                                  onClick={handleOpenNavMenu}
-                                  color="inherit"
-                              >
-                                  <MenuIcon />
-                              </IconButton>
-                              <Menu
-                                  id="menu-appbar"
-                                  anchorEl={anchorElNav}
-                                  anchorOrigin={{
-                                      vertical: "bottom",
-                                      horizontal: "left",
-                                  }}
-                                  keepMounted
-                                  transformOrigin={{
-                                      vertical: "top",
-                                      horizontal: "left",
-                                  }}
-                                  open={Boolean(anchorElNav)}
-                                  onClose={handleCloseNavMenu}
-                                  sx={{
-                                      display: { xs: "block", md: "none" },
-                                  }}
-                              >
-                                  {pages.map((page) => (
-                                      <MenuItem
-                                          key={page}
-                                          onClick={handleCloseNavMenu}
-                                      >
-                                          <Typography textAlign="center">
-                                              {page}
-                                          </Typography>
-                                      </MenuItem>
-                                  ))}
-                              </Menu>
-                          </Box>
-                          <HomeIcon
-                              sx={{
-                                  display: { xs: "flex", md: "none" },
-                                  mr: 1,
-                              }}
-                          />
-                          <Typography
-                              variant="h5"
-                              noWrap
-                              component="a"
-                              href=""
-                              sx={{
-                                  mr: 2,
-                                  display: { xs: "flex", md: "none" },
-                                  flexGrow: 1,
-                                  fontFamily: "monospace",
-                                  fontWeight: 700,
-                                  letterSpacing: ".3rem",
-                                  color: "inherit",
-                                  textDecoration: "none",
-                              }}
-                          >
-                              不動産（仮）
-                          </Typography>
-                          {/* レスポンシブ */}
-                          {/* 通常 */}
-                          <Box
-                              sx={{
-                                  justifyContent: "flex-end",
-                                  flex: 1,
-                                  mr: 2,
-                                  gap: 2,
-                                  display: { xs: "none", md: "flex" },
-                              }}
-                          >
-                              {pages.map((page) => (
-                                  <Button
-                                      key={page}
-                                      onClick={handleCloseNavMenu}
-                                      sx={{
-                                          my: 1,
-                                          color: "white",
-                                          display: "block",
-                                      }}
-                                  >
-                                      {page}
-                                      <Box>0件</Box>
-                                  </Button>
-                              ))}
-                          </Box>
-                          {/* 通常 */}
-                          {/* 共通 */}
-                          <Box sx={{ flexGrow: 0 }}>
-                              <Tooltip title="Open settings">
-                                  <IconButton
-                                      onClick={handleOpenUserMenu}
-                                      sx={{ p: 0 }}
-                                  >
-                                      <Avatar
-                                          alt="Remy Sharp"
-                                          src="/static/images/avatar/2.jpg"
-                                      />
-                                  </IconButton>
-                              </Tooltip>
-                              <Menu
-                                  sx={{ mt: "45px" }}
-                                  id="menu-appbar"
-                                  anchorEl={anchorElUser}
-                                  anchorOrigin={{
-                                      vertical: "top",
-                                      horizontal: "right",
-                                  }}
-                                  keepMounted
-                                  transformOrigin={{
-                                      vertical: "top",
-                                      horizontal: "right",
-                                  }}
-                                  open={Boolean(anchorElUser)}
-                                  onClose={handleCloseUserMenu}
-                              >
-                                  {settings.map((setting) => (
-                                      <MenuItem
-                                          key={setting}
-                                          onClick={handleCloseUserMenu}
-                                      >
-                                          <Typography textAlign="center">
-                                              {setting}
-                                          </Typography>
-                                      </MenuItem>
-                                  ))}
-                              </Menu>
-                          </Box>
-                          {/* 共通 */}
-                      </Toolbar>
+
+    <ThemeProvider theme={defaultTheme}>
+        <Header />
+        <div className={styles.mainVisual}>
+        <Container
+                      maxWidth="md"
+                      sx={{ backgroundColor: "aliceblue", py: 4 }}
+                  >
+                      <Typography variant="h5">
+                          Welcome to eBuye!
+                      </Typography>
+                      <Typography>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Quia corrupti quos reiciendis error nulla veniam
+                          repellendus aut temporibus sapiente itaque dolore
+                          molestiae quas debitis dolores cupiditate, voluptatem
+                          tempora. Veritatis, voluptate!
+                      </Typography>
                   </Container>
-              </AppBar>
-          </div>
-          <div className={classes.content}>
+        {/* <img src="/images/main.jpg" className={styles.mainImg}></img> */}
+        </div>
+      <Container >
+        <main className={styles.main}>
+          <div className={styles.content}>
               <Box
                   sx={{
                       display: "flex",
                       flexDirection: "column",
-                      my: 10,
-                      gap: 10,
+                    //   my: 10,
+                    //   gap: 10,
                   }}
               >
                   <Container
                       maxWidth="md"
-                      sx={{ backgroundColor: "white", py: 4 }}
+                      sx={{ backgroundColor: "aliceblue", py: 4 }}
                   >
                       <Typography variant="h5">
-                          福岡の住まい探しは。。。
+                          Welcome to eBuye!
                       </Typography>
                       <Typography>
                           Lorem ipsum dolor sit amet consectetur adipisicing
@@ -256,13 +81,13 @@ export const Top_copy = () => {
                   <Container
                       maxWidth="md"
                       sx={{
-                          backgroundColor: "red",
+                          backgroundColor: "aliceblue",
                           py: 4,
                           display: "flex",
                           flexDirection: "column",
                       }}
                   >
-                      <Typography variant="h5">タイトル2</Typography>
+                      <Typography variant="h5">Estates</Typography>
                       <Box sx={{ display: "flex" }}>
                           <Box
                               sx={{
@@ -310,45 +135,28 @@ export const Top_copy = () => {
                                               width: 160,
                                           }}
                                       >
-                                          <Typography variant="h6" >
+                                        <Link to={i.url}>
+                                          <Typography variant="h6">
                                               {i.title}
                                           </Typography>
                                           <Box>{i.text}</Box>
+                                        </Link>
                                       </Box>
                                   );
                               })}
                           </Box>
                       </Box>
                   </Container>
-                  <Container
-                      maxWidth="md"
-                      sx={{ backgroundColor: "green", py: 4 }}
-                  >
-                      <Typography variant="h5">タイトル3</Typography>
-                      <Typography>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Quia corrupti quos reiciendis error nulla veniam
-                          repellendus aut temporibus sapiente itaque dolore
-                          molestiae quas debitis dolores cupiditate, voluptatem
-                          tempora. Veritatis, voluptate!
-                      </Typography>
-                  </Container>
-                  <Container
-                      maxWidth="md"
-                      sx={{ backgroundColor: "blue", py: 4 }}
-                  >
-                      <Typography variant="h5">タイトル4</Typography>
-                      <Typography>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Quia corrupti quos reiciendis error nulla veniam
-                          repellendus aut temporibus sapiente itaque dolore
-                          molestiae quas debitis dolores cupiditate, voluptatem
-                          tempora. Veritatis, voluptate!
-                      </Typography>
-                  </Container>
+                  
               </Box>
           </div>
-          <div className={classes.footer}>フッター要素</div>
-      </div>
+
+        </main>
+      </Container>
+      <Footer
+        title="Footer"
+        description="Something here to give the footer a purpose!"
+      />
+    </ThemeProvider>
   );
-};
+            }
--- src/resources/js/pages/Top.jsx
+++ src/resources/js/pages/Top.jsx
@@ -1,149 +1,77 @@
 import React from "react";
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { Link } from 'react-router-dom';
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
-import classes from "../../sass/top.module.scss";
-
+import styles from "../../sass/top.module.scss";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
 
-import DBtest from '../api/DBtest';
-
-
-const dummy = [
-    [
-        { id: 1, url:"/Search", title: "住まい", text: "00000件" },
-        { id: 2, url:"/Search", title: "店舗・事務所", text: "11111件" },
-        { id: 3, url:"/Search", title: "その他", text: "22222件" },
-        { id: 4, url:"/Search", title: "駐車場", text: "33333件" },
-    ],
-    [
-        { id: 5, url:"/Search", title: "戸建", text: "55555件" },
-        { id: 6, url:"/Search", title: "マンション", text: "66666件" },
-        { id: 7, url:"/Search", title: "土地", text: "777件" },
-        { id: 8, url:"/Search", title: "その他", text: "8件" },
-    ],
-];
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
+
 export default function Top() {
   return (
-
     <ThemeProvider theme={defaultTheme}>
-      
-      <CssBaseline />
-      <Container >
-        <Header />
-        <main>
-          {/* <DBtest /> */}
-          <div className={classes.content}>
-              <Box
-                  sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                    //   my: 10,
-                    //   gap: 10,
-                  }}
-              >
-                  <Container
-                      maxWidth="md"
-                      sx={{ backgroundColor: "aliceblue", py: 4 }}
+      <Header />
+      <div className={styles.mainVisual}>
+        {/* 背景画像の上に要素を配置 */}
+        <div className={styles.mainMask}>
+          <Container maxWidth="md">
+            <div className={styles.buttonsContainer}>
+              <div className={styles.buttons}>
+                <p className={styles.p}>福岡の不動産・賃貸情報</p>
+                <p className={styles.p}>どのような物件をお探しですか？</p>
+                <div className={styles.buttonGroup}>
+                <Link to="/Search_area">
+                  <Button
+                    variant="contained"
+                    color="warning"
+                    sx={{
+                      width: "150px",
+                      height: "150px",
+                      mr: 3,
+                      backgroundColor:"#ffc107",
+                      fontSize:"1.2em",
+                    }}
+                    // href="/Search_area"
                   >
-                      <Typography variant="h5">
-                          Welcome to eBuye!
-                      </Typography>
-                      <Typography>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Quia corrupti quos reiciendis error nulla veniam
-                          repellendus aut temporibus sapiente itaque dolore
-                          molestiae quas debitis dolores cupiditate, voluptatem
-                          tempora. Veritatis, voluptate!
-                      </Typography>
-                  </Container>
-                  <Container
-                      maxWidth="md"
-                      sx={{
-                          backgroundColor: "aliceblue",
-                          py: 4,
-                          display: "flex",
-                          flexDirection: "column",
-                      }}
+                    借りる
+                  </Button>
+                </Link>
+                <Link to="/Search_area">
+                  <Button
+                    variant="contained"
+                    color="warning"
+                    sx={{
+                      width: "150px",
+                      height: "150px",
+                      backgroundColor:"#ffc107",
+                      fontSize:"1.2em",
+                    }}
                   >
-                      <Typography variant="h5">Estates</Typography>
-                      <Box sx={{ display: "flex" }}>
-                          <Box
-                              sx={{
-                                  display: "flex",
-                                  flexWrap: "wrap",
-                                  gap: 2,
-                                  justifyContent: "center",
-                              }}
-                          >
-                            
-                              {dummy[0].map((i) => {
-                                  return (
-                                    <Box
-                                          key={i.id}
-                                          sx={{
-                                            backgroundColor: "white",
-                                            width: 160,
-                                          }}
-                                      >
-                                        <Link to={i.url}>
-                                          <Typography variant="h6">
-                                              {i.title}
-                                          </Typography>
-                                          <Box>{i.text}</Box>
-                                        </Link>
-                                      </Box>
-
-                                  );
-                              })}
-                          </Box>
-                          <Box
-                              sx={{
-                                  display: "flex",
-                                  flexWrap: "wrap",
-                                  gap: 2,
-                                  justifyContent: "center",
-                              }}
-                          >
-                              {dummy[1].map((i) => {
-                                  return (
-                                      <Box
-                                          key={i.id}
-                                          sx={{
-                                              backgroundColor: "white",
-                                              width: 160,
-                                          }}
-                                      >
-                                        <Link to={i.url}>
-                                          <Typography variant="h6">
-                                              {i.title}
-                                          </Typography>
-                                          <Box>{i.text}</Box>
-                                        </Link>
-                                      </Box>
-                                  );
-                              })}
-                          </Box>
-                      </Box>
-                  </Container>
-                  
-              </Box>
-          </div>
-
+                    買う
+                  </Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+      </div>
+      <Container>
+        <main className={styles.main}>
+          <div className={styles.content}></div>
         </main>
       </Container>
       <Footer
         title="Footer"
         description="Something here to give the footer a purpose!"
+        sx={{ mt: "auto" }}
       />
     </ThemeProvider>
   );
-            }
+}
